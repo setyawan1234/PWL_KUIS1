32d708448b97780f6befaf43fdb4327eb95d23c8
setyawan1234 <twentyzee1@gmail.com>
2022-03-18 16:21:22 +0700

    mengisi pelanggan controller

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\pelanggan;
 use Illuminate\Http\Request;
 
 class PelangganController extends Controller
 {
-    //
+    public function pelanggan(){
+    $data = pelanggan::all();
+        return view ('pelanggan',['pelanggan'=>$data])
+        ->with('title','Customer');
+    }
 }
