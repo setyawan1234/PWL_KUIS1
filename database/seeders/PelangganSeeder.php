94ee229d41868544a01d98767bc19d774ce22d82
setyawan1234 <twentyzee1@gmail.com>
2022-03-18 21:39:26 +0700

    update 3 pelanggan seeder

--- database/seeders/PelangganSeeder.php
+++ database/seeders/PelangganSeeder.php
@@ -17,98 +17,98 @@ class PelangganSeeder extends Seeder
         $data =[
             [
                 'nama' => 'Mahisa Budha Nasution',
-                'jenisKelamin' => 'Laki-Laki',
+                'jenisKelamin' => 'https://cdn3.iconfinder.com/data/icons/car-dealership/512/Customer-512.png',
                 'alamat' => 'Mojokerto',
                 'email' => 'mahisa123@gmail.com',
                 'noTelp' => '085018018018'
             ],
             [
                 'nama' => 'Aghisni Fadli Nasution',
-                'jenisKelamin' => 'Laki-Laki',
+                'jenisKelamin' => 'https://cdn3.iconfinder.com/data/icons/car-dealership/512/Customer-512.png',
                 'alamat' => 'Bangil',
                 'email' => 'Nasution9098@gmail.com',
                 'noTelp' => '085123123123'
             ],
             [
                 'nama' => 'Enjel Setia Ningsih',
-                'jenisKelamin' => 'Perempuan',
+                'jenisKelamin' => 'https://cdn.pixabay.com/photo/2021/02/12/07/05/woman-6007535_1280.png',
                 'alamat' => 'Bandung',
                 'email' => 'EnjelNingsih334@gmail.com',
                 'noTelp' => '085234234234'
             ],
             [
                 'nama' => 'Clara Keyla',
-                'jenisKelamin' => 'Perempuan',
+                'jenisKelamin' => 'https://cdn.pixabay.com/photo/2021/02/12/07/05/woman-6007535_1280.png',
                 'alamat' => 'Kediri',
                 'email' => 'Keyra@gmail.com',
                 'noTelp' => '085345345345'
             ],
             [
                 'nama' => 'Aurora Neisya',
-                'jenisKelamin' => 'Perempuan',
+                'jenisKelamin' => 'https://cdn.pixabay.com/photo/2021/02/12/07/05/woman-6007535_1280.png',
                 'alamat' => 'Sidoarjo',
                 'email' => 'rasya@gmail.com',
                 'noTelp' => '085456456456'
             ],
             [
                 'nama' => 'Suci Ardila',
-                'jenisKelamin' => 'Perempuan',
+                'jenisKelamin' => 'https://cdn.pixabay.com/photo/2021/02/12/07/05/woman-6007535_1280.png',
                 'alamat' => 'Magelang',
                 'email' => 'suci@gmail.com',
                 'noTelp' => '085567567567'
             ],
             [
                 'nama' => 'Imannuel Sondaqh',
-                'jenisKelamin' => 'Laki-Laki',
+                'jenisKelamin' => 'https://cdn3.iconfinder.com/data/icons/car-dealership/512/Customer-512.png',
                 'alamat' => 'Jakarta',
                 'email' => 'levi@gmail.com',
                 'noTelp' => '085678678678'
             ],
             [
                 'nama' => 'Mahardika Bagus',
-                'jenisKelamin' => 'Laki-Laki',
+                'jenisKelamin' => 'https://cdn3.iconfinder.com/data/icons/car-dealership/512/Customer-512.png',
                 'alamat' => 'Kediri',
                 'email' => 'bagus@gmail.com',
                 'noTelp' => '085789789789'
             ],
             [
                 'nama' => 'Brian Farel',
-                'jenisKelamin' => 'Laki-Laki',
+                'jenisKelamin' => 'https://cdn3.iconfinder.com/data/icons/car-dealership/512/Customer-512.png',
                 'alamat' => 'Kediri',
                 'email' => 'frrl@gmail.com',
                 'noTelp' => '085890890890'
             ],
             [
                 'nama' => 'Agatha Raya',
-                'jenisKelamin' => 'Laki-Laki',
+                'jenisKelamin' => 'https://cdn3.iconfinder.com/data/icons/car-dealership/512/Customer-512.png',
                 'alamat' => 'Mojokerto',
                 'email' => 'ray@gmail.com',
                 'noTelp' => '085112112112'
             ],
             [
                 'nama' => 'Rizky Alamsyah',
-                'jenisKelamin' => 'Laki-Laki',
+                'jenisKelamin' => 'https://cdn3.iconfinder.com/data/icons/car-dealership/512/Customer-512.png',
                 'alamat' => 'Pacet',
                 'email' => 'rizky@gmail.com',
                 'noTelp' => '085122122122'
             ],
             [
                 'nama' => 'Anthares Zidni',
-                'jenisKelamin' => 'Perempuan',
+                'jenisKelamin' => 'https://cdn.pixabay.com/photo/2021/02/12/07/05/woman-6007535_1280.png',
                 'alamat' => 'Bandung',
                 'email' => 'zidni@gmail.com',
                 'noTelp' => '085133133133'
             ],
             [
                 'nama' => 'Salsabila Khares',
-                'jenisKelamin' => 'Perempuan',
+                'jenisKelamin' => 'https://cdn.pixabay.com/photo/2021/02/12/07/05/woman-6007535_1280.png',
                 'alamat' => 'Lamongan',
                 'email' => 'salsa@gmail.com',
                 'noTelp' => '085143143143'
             ],
             [
                 'nama' => 'Hanifa Nur Rahman',
-                'jenisKelamin' => 'Laki-Laki',
+                'jenisKelamin' => 'https://cdn3.iconfinder.com/data/icons/car-dealership/512/Customer-512.png',
                 'alamat' => 'Mojokerto',
                 'email' => 'ifa@gmail.com',
                 'noTelp' => '085432432432'
