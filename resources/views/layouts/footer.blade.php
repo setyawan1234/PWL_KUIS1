a7c9d1fa7f18f0d7e14fff458ac55e95ec563319
setyawan1234 <twentyzee1@gmail.com>
2022-03-18 20:21:37 +0700

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,40 @@
+<div class="blue-divider effect1">
+    <div class="dark-blue-bg"></div>
+    <div class="blue-divider-bg-graphic"></div>
+    <div class="blue-bg"></div>
+</div>
+<section class="templatemo-container blue-bg footer-nav effect1">
+    <div class="container">
+        <div class="col-lg-2 col-md-2 col-sm-6 footer-block">
+            <h3 class="text-uppercase">Main menu</h3>
+            <nav class="text-uppercase templatemo-nav-2">
+                <ul>
+                    <li><a href="/home" >Home</a></li>
+                    <li><a href="/product">product</a></li>
+                    <li><a href="/customer">customer</a></li>
+                    <li><a href="/employee">empployee</a></li>
+                    <li><a href="/supplier">supplier</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6 footer-block">
+            <h3 class="text-uppercase">About us</h3>
+            <p>HI-TECH MALL adalah a website that introduces a laptop product from a well-known brand to make it easier for users to search .</p>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 footer-block">
+            <h3 class="text-uppercase">Resources</h3>
+            <ul>
+                <li><a href="#">HI-Tech.com</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 footer-block">
+            <h3 class="text-uppercase">Features</h3>
+            <ul>
+                <li><a href="#"></a>Type Laptop</li>
+                <li><a href="#"></a>Type Accesories</li>
+            </ul>
+        </div>
+</section>
 <footer class="text-center">
     <p class="small copyright-text">Copyright &copy;HI TECH MALL</p>
 </footer>
