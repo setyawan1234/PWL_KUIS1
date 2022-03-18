e87a2489e71be94a5dde8d2cf9912a5c9d665c9b
setyawan1234 <twentyzee1@gmail.com>
2022-03-18 09:20:31 +0700

    Halaman Home

---
+++ resources/views/index.blade.php
@@ -0,0 +1,225 @@
+@extends('layouts.main')
+@section('main')
+<body>
+
+    <!-- header image -->
+    <div class="header-img"></div>
+    <!-- end header image -->
+
+    <!-- Welcome message -->
+    <div class="welcome-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="welcome-wrapper">
+                        <h2 class="welcome-title text-uppercase">HI-TECH MALL</h2>
+                        <img src="asset/img/welcome-divider-lines.png" alt="Welcome divider" class="welcome-divider-lines-img">
+                        <p class="welcome-description">HI-TECH MALL presents a new laptop from the LENOVO brand with the latest type LEGION 5i PRO</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Welcome message -->
+    
+    <!-- main content -->
+    <section class="templatemo-container section-shadow-bottom">
+        <div class="container">
+            <div class="row section-title-container">
+                <div class="col-lg-12 text-uppercase text-center">
+                    <h2 class="section-title">HI-TECH</h2>
+                    <div class="section-title-underline-blue"></div>
+                    <hr class="section-title-underline">
+                    <p class="small">Lenovo Legion 5i Pro</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <h3 class="section-title-2 text-uppercase font-weight-300"><b>Legion</b> <span class="blue-text">5i Pro</span></h3>
+                    <p>Lenovo presents a new laptop with fast specifications. This laptop is included in the Legion class. The colors that have been given by ASUS are red, black, white</p>
+                    <ul class="gray-text ul-1">
+                        <li>Up to 12th Gen Intel® Core™ i9-12900H.</li>
+                        <li>NVIDIA® GeForce RTX™ 3070 Ti 8GB GDDR6 (150W TGP, Listed Boost clock: 1485Mhz).</li>
+                        <li>Windows 11 Home.</li>
+                    </ul>
+                    <a href="#" class="btn-blue-gradient">Lenovo</a>
+                    <a href="#" class="btn-transparent">Legion 5I PRO</a>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="carousel-container">
+                        <div class="crsl-items" data-navigation="navbtns">
+                            <div class="crsl-wrap">
+                                <figure class="crsl-item">
+                                    <img src="https://img.tek.id/img/content/2021/04/06/39321/sah-lenovo-legion-5-pro-dan-legion-7-resmi-melenggang-di-indonesia-kRfD7im6L6.jpg" alt="Image" class="img-responsive img-thumbnail">
+                                </figure>
+                                <figure class="crsl-item">
+                                    <img src="https://pemmz.com/pub/media/catalog/product/cache/bd3f5a515fc184d023496aa6e56fb6f6/l/e/lenovo_legion_5i_pro_16ith6h_3_.jpg" alt="Image" class="img-responsive img-thumbnail">
+                                </figure>
+                                <figure class="crsl-item">
+                                    <img src="https://p2-ofp.static.pub/fes/cms/2021/06/23/cnyo7v9yv63dh15a6rl3xxzu71iffq095542.jpg" alt="Image" class="img-responsive img-thumbnail">
+                                </figure>
+                            </div>
+                        </div>
+                    </div>   <!-- carousel-container -->
+                    <div id="navbtns" class="crsl-nav">
+                        <a href="#" class="previous">
+                            <img src="asset/img/carousel-arrow-previous.png" alt="previous">
+                        </a>
+                        <a href="#" class="next">
+                            <img src="asset/img/carousel-arrow-next.png" alt="next">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- end section content -->
+        </div>
+    </section>
+    <!-- Who We Are -->
+    <section class="templatemo-container light-gray-bg section-shadow-bottom">
+        <div class="container">
+            <div class="row section-title-container">
+                <div class="col-lg-12 text-uppercase text-center">
+                    <h2 class="section-title">HI-TECH MALL</h2>
+                    <div class="section-title-underline-blue"></div>
+                    <hr class="section-title-underline">
+                    <p class="small">Lenovo Legion 5i Pro</p>
+                </div>
+            </div> <!-- row -->
+            <div class="row">
+                <div class="col-lg-5 col-md-5">
+                    <div class="tm-blocks-container effect1">
+                        <div class="tm-block green-bg">
+                            <a href="#" class="tm-block-link">Lenovo</a>
+                        </div>
+                        <div class="tm-block">
+                            <img src="https://www.lenovo.com/medias/Legion-5i-Pro-Gen6-16-11th-i5i7.png?context=bWFzdGVyfHJvb3R8NjQxNjN8aW1hZ2UvcG5nfGg0OS9oZjEvMTI2OTU3MjA2MjQxNTgucG5nfGQyN2YzNWU5ZTMyMzRjMzliM2JlOTJiMzIxYmU0NTg0YWEyODIyNDMzNDg1OTRhNDc1MjdlYTliOGUwNGMzODQ" alt="Image" class="img-responsive">
+                        </div>
+                        <div class="tm-block">
+                            <img src="https://www.harapanrakyat.com/wp-content/uploads/2021/04/12.-Lenovo-Legion-5-Pro-Laptop-Gaming-yang-Telah-Hadir-Bersama-Legion-7.jpg" alt="Image" class="img-responsive">
+                        </div>
+                        <div class="tm-block red-bg">
+                            <a href="#" class="tm-block-link">Legion</a>
+                        </div>
+                        <div class="tm-block yellow-bg">
+                            <a href="#" class="tm-block-link">Gaming 5i Pro</a>
+                        </div>
+                        <div class="tm-block">
+                            <img src="https://p-id.ipricegroup.com/uploaded_d710d3a9a1f7ade370db64214e4d1e04.jpg" alt="Image" class="img-responsive">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-7 col-md-7">
+                    <h3 class="section-title-2 text-uppercase font-weight-300"><b>Lenovo Legion 5i Pro</b> <span class="blue-text">SPECIFICATION</span></h3>
+                    <p class="justify-text">* Processor : Intel Core I5 GEN 12Th <br> * RAM : 16 GB DDR 4 <br> * SSD : 512 GB SSD <br> * Microsoft : Home And Studio </p>
+                    <p class="justify-text">
+                        * VGA : Nvidia @GeForce RTX™ 3070 8GB GDDR6 <br> * DISPLAY : 15,6 inch FHD(1920 X 1200) 16:10 anti-glare sRGB : 100% <br> * PORT/IO : 1x3.5mm Combo Audio Jack <br> * OS : Windows 11 Home
+                    </p>
+                    <p class="justify-text">
+                        * KEYBOARD AND TOUCHPAD : Backlit Chiclet Keyboard 4-Zone RGB Touchpad <br> * AUDIO : Audio By Dolby Atmos <br> * BATTERY : 56WHrs, 4SIP, 4-cell Li-ion <br> * Weight : 2.10 Kg   
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog -->
+    <section class="templatemo-container">
+        <div class="container">
+            <div class="row section-title-container">
+                <div class="col-lg-12 text-uppercase text-center">
+                    <h2 class="section-title">Blog</h2>
+                    <div class="section-title-underline-blue"></div>
+                    <hr class="section-title-underline">
+                    <p class="small">LENOVO LEGION 5I PRO</p>
+                </div>
+            </div>
+            <div class="row posts-container">
+                <div class="post-excerpt post-excerpt-left">
+                    <div class="post-content">
+                        <h3 class="post-title">Alpha Pramadhani (04)</h3>
+                        <p class="post-description">Jurusan Teknologi Informasi Prodi D-IV Teknik Informatika</p>
+                    </div>
+                    <div class="post-date-container">
+                        <div class="post-month">Maret</div>
+                        <div class="post-date">17</div>
+                    </div>
+                    <div class="post-arrow-container rotate-arrow">
+                        <img src="asset/img/blog-arrow.png" alt="arrow">
+                    </div>
+                </div>
+                <div class="post-excerpt post-excerpt-right">
+                    <div class="post-arrow-container">
+                        <img src="asset/img/blog-arrow.png" alt="arrow">
+                    </div>
+                    <div class="post-date-container">
+                        <div class="post-month">Maret</div>
+                        <div class="post-date">17</div>
+                    </div>
+                    <div class="post-content">
+                        <h3 class="post-title">Gilang Setyawan (12)</h3>
+                        <p class="post-description">Jurusan Teknologi Informasi Prodi D-IV Teknik Informatika</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <div class="blue-divider effect1">
+        <div class="dark-blue-bg"></div>
+        <div class="blue-divider-bg-graphic"></div>
+        <div class="blue-bg"></div>
+    </div>
+    <section class="templatemo-container blue-bg footer-nav effect1">
+        <div class="container">
+            <div class="col-lg-2 col-md-2 col-sm-6 footer-block">
+                <h3 class="text-uppercase">Main menu</h3>
+                <nav class="text-uppercase templatemo-nav-2">
+                    <ul>
+                        <li><a href="/home" >Home</a></li>
+                        <li><a href="/product">product</a></li>
+                        <li><a href="/customer">customer</a></li>
+                        <li><a href="/employee">empployee</a></li>
+                        <li><a href="/supplier">supplier</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 footer-block">
+                    <h3 class="text-uppercase">About us</h3>
+                    <p>HI-TECH MALL adalah a website that introduces a laptop product from a well-known brand to make it easier for users to search .</p>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 footer-block">
+                    <h3 class="text-uppercase">Resources</h3>
+                    <ul>
+                        <li><a href="#">HI-Tech.com</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 footer-block">
+                    <h3 class="text-uppercase">Features</h3>
+                    <ul>
+                        <li><a href="#"></a>Type Laptop</li>
+                        <li><a href="#"></a>Type Accesories</li>
+                    </ul>
+                </div>
+    </section>
+    <!-- end main content -->
+
+    <!-- JS -->
+    <script type="text/javascript" src="asset/js/jquery-1.11.2.min.js"></script>      <!-- jQuery -->
+    <script type="text/javascript" src="asset/js/responsiveCarousel.min.js"></script>      <!-- Carousel -->
+    <script type="text/javascript" src="asset/js/templatemo-script.js"></script>      <!-- Templatemo Script -->
+    <script>
+
+        $(function() {
+            $('.crsl-items').carousel({
+                visible: 1,
+                itemMinWidth: 320,
+                itemEqualHeight: 320,
+                itemMargin: 9,
+            });
+            $(".crsl-nav a[href=#]").on('click', function(e) {
+                e.preventDefault();
+            });
+        });
+
+    </script>
+</body>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +21,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
