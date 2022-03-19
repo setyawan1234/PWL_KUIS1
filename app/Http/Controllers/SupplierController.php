9528e6da19d32e100774c6ebd7bf95bd56e3b954
AlphaP45 <alphapramadhani45@gmail.com>
2022-03-19 20:48:28 +0700

    update controller supplier

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\supplier;
 use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
     //
+    public function supplier(){
+        $data = supplier::all();
+            return view ('supplier',['supplier'=>$data])
+            ->with('title','supplier');
+        }
 }
