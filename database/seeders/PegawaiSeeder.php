2ddda02b6521ea3e60ad820d78665ccd79353e94
AlphaP45 <alphapramadhani45@gmail.com>
2022-03-19 20:49:27 +0700

    update seeder pegawai

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -19,18 +19,21 @@ class PegawaiSeeder extends Seeder
             [
                 'namaPegawai' => 'Rozak',
                 'jenisKelamin' => 'Laki-Laki',
+                'jabatan' => 'Manajer Toko',
                 'alamat' => 'Mojokerto',
                 'noTelp' => '081223445501'
             ],
             [
                 'namaPegawai' => 'Zaki',
                 'jenisKelamin' => 'Laki-Laki',
+                'jabatan' => 'Kasir',
                 'alamat' => 'Malang',
                 'noTelp' => '081332544000'
             ],
             [
                 'namaPegawai' => 'Rahmawati',
                 'jenisKelamin' => 'Perempuan',
+                'jabatan' => 'Sales',
                 'alamat' => 'Malang',
                 'noTelp' => '081443556345'
             ]
