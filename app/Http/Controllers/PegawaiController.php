62f33788bacc3f7c6e9b487513906418fc7cf577
AlphaP45 <alphapramadhani45@gmail.com>
2022-03-19 20:48:13 +0700

    update controller pegawai

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\pegawai;
 use Illuminate\Http\Request;
 
 class PegawaiController extends Controller
 {
     //
+    public function pegawai(){
+        $data = pegawai::all();
+            return view ('pegawai',['pegawai'=>$data])
+            ->with('title','empployee');
+        }
 }
