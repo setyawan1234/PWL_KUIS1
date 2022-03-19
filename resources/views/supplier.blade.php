40a9f30487c2b82cd7463ed2b9a5ef30afc43562
AlphaP45 <alphapramadhani45@gmail.com>
2022-03-19 20:50:10 +0700

    update blade supplier

---
+++ resources/views/supplier.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.main')
+@section('main')
+<body>
+    <section class="templatemo-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-uppercase text-center">
+                    <h2 class="section-title">LIST Supplier</h2>
+                    <div class="section-title-underline-blue"></div>
+                    <hr class="section-title-underline">
+                    <p class="small">Hi-Tech Mall Website</p>
+                </div>
+            </div>
+    </section>
+    <section>
+        <div class="container tm-position-relative">
+            <div class="row">
+                <table class="table">
+                    <thead>
+                            <th>Id</th>
+                            <th>NamaSupplier</th>
+                            <th>Produk</th>
+                            <th>Kota</th>
+                            <th>No Telepon</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($supplier as $p)
+                            <tr>
+                                <td>{{$p->id}}</td>
+                                <td>{{$p->namaSupplier}}</td>
+                                <td>{{$p->produk}}</td>
+                                <td>{{$p->kota}}</td>
+                                <td>{{$p->noTelp}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+</body>
+@endsection
